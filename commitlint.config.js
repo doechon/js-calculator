9d58538f9696f443dfecabd4039019cc45504bd5
get <ivanchebykin4@gmail.com>
2025-11-07 22:03:56 +0500

    build: настроить инфраструктуру проекта

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
-module.exports = {
+export default {
     extends: ["@commitlint/config-conventional"],
     rules: {
-        "type-enum": [2, "always", ["feat", "bug", "fix", "refactor"]],
+        "type-enum": [2, "always", ["feat", "fix", "refactor"]],
     },
 }
