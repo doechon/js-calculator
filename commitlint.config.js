54d62f7a0b29559794bebbaa30d5b70b107547c7
get <ivanchebykin4@gmail.com>
2025-11-07 23:17:58 +0500

    chore: настройка окружения, добавлены task

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,7 @@ export default {
     extends: ["@commitlint/config-conventional"],
     rules: {
         "type-enum": [2, "always", ["feat", "fix", "refactor", "build", "chore"]],
+        // НОВОЕ ПРАВИЛО: feat и fix должны иметь номер тикета
+        "subject-ticket-format": [2, "always", /^(feat|fix): #\d+ .+$|^(refactor|build|chore): .+$/],
     },
 }
